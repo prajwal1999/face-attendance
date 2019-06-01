ed00bfeac8f6becc251fe7fb9c8dabf0e2576621
prajwal1999 <prajwaldkamble1998@gmail.com>
2019-06-02 00:50:59 +0530

    version 0.0.3

--- index.js
+++ index.js
@@ -14,7 +14,15 @@ faceapi.env.monkeyPatch({
 Promise.all([
     faceapi.nets.tinyFaceDetector.loadFromUri('./src/ML-Api/weights/')
   ]).then(()=>{
-    console.log('loaded')
+    // setInterval(async ()=>{ console.log(1)
+    //   const a = await checkSingleFrame()
+    //   try {
+    //     console.log(a)
+    //   } catch (error) {
+    //     console.log(error)
+    //   }
+    //   console.log(1)
+    // })
   })
 
 const startVideo = async()=>{
@@ -34,19 +42,39 @@ const startVideo = async()=>{
     )
   }
   
-  startVideo()
+startVideo()
 
-  const onPlay =  async ()=> { 
+  const onPlay =  async ()=> {
     if(videoEl.paused || videoEl.ended || !isFaceDetectionModelLoaded())
       return setTimeout(() => onPlay())
 
     const options = getFaceDetectorOptions()
+    
     const result = await faceapi.detectSingleFace(videoEl, options)
-    if (result) {
-        // console.log(result)
+    if (result) { 
         const canvas = $('#overlay').get(0)
         const dims = faceapi.matchDimensions(canvas, videoEl, true)
         faceapi.draw.drawDetections(canvas, faceapi.resizeResults(result, dims))
+        if(result.box.width > 330){
+          const canvas = document.createElement('canvas')
+            canvas.width = videoEl.videoWidth
+            canvas.height = videoEl.videoHeight
+            canvas.getContext('2d').drawImage(videoEl, 0, 0)
+          let temp_img = document.createElement('img')
+            temp_img.src = canvas.toDataURL('image/jpeg')
+          const _x = result.box['x']-result.box['width']/20
+          const _y = result.box['y']-result.box['height']/20
+          const _width = result.box['width']*1.1
+          const _height = result.box['height']*1.1
+          let crop_canvas = document.createElement('canvas')
+            crop_canvas.width = _width
+            crop_canvas.height = _height
+          let ctx = crop_canvas.getContext('2d')
+            ctx.drawImage(temp_img, _x, _y, _width, _height, 0, 0, _width, _height)
+          let croppedImgdata = crop_canvas.toDataURL('image/jpeg')
+          const obj_to_return = {status: true, imgData: croppedImgdata}
+          console.log(obj_to_return)
+        }
     }
     else {
         // console.log('result is not available')
@@ -56,90 +84,67 @@ const startVideo = async()=>{
     setTimeout(() => onPlay())
   }
 
-  const setImageName = async ()=>{
-    try {
-    //   const storeId = await localStorage.getItem("storeId")
-      const storeId = "THN003"
-      let d = new Date()
-      let year = d.getUTCFullYear().toString()
-      let month = d.getUTCMonth() + 1
-        if(month < 10){month = '0'+month.toString()}
-        else{month = month.toString()}
-      let date = d.getUTCDate().toString()
-      let hours = d.getUTCHours()
-      let minute = d.getUTCMinutes()
-      let second = d.getUTCSeconds()
-      return storeId.toString() + date + month + year + hours + minute + second
-    } catch (error) {
-      return false
+
+const checkSingleFrame = async ()=>{
+  const options = getFaceDetectorOptions()
+  const canvas = document.createElement('canvas')
+    canvas.width = videoEl.videoWidth;
+    canvas.height = videoEl.videoHeight;
+    canvas.getContext('2d').drawImage(videoEl, 0, 0);
+  let temp_img = document.createElement('img');
+    temp_img.src = canvas.toDataURL('image/jpeg');
+  const image_result = await faceapi.detectSingleFace(temp_img, options)
+  return new Promise((resolve, reject)=>{
+    if(image_result && image_result.box.width > 300){
+      const _x = image_result.box['x']-image_result.box['width']/20
+      const _y = image_result.box['y']-image_result.box['height']/20
+      const _width = image_result.box['width']*1.1
+      const _height = image_result.box['height']*1.1     
+      let crop_canvas = document.createElement('canvas')
+        crop_canvas.width = _width
+        crop_canvas.height = _height
+      let ctx = crop_canvas.getContext('2d')
+        ctx.drawImage(temp_img, _x, _y, _width, _height, 0, 0, _width, _height)
+      let croppedImgdata = crop_canvas.toDataURL('image/jpeg')
+      const obj_to_return = {status: true, imgData: croppedImgdata}
+      resolve(obj_to_return)
+    } else{
+      reject(false)
     }
-  }
+  })
+}
 
-  const checkSingleFrame = async ()=>{
-    const options = getFaceDetectorOptions()
-    const canvas = document.createElement('canvas');
-      canvas.width = videoEl.videoWidth;
-      canvas.height = videoEl.videoHeight;
-      canvas.getContext('2d').drawImage(videoEl, 0, 0);
-    let temp_img = document.createElement('img');
-      temp_img.src = canvas.toDataURL('image/jpeg');
-    const image_result = await faceapi.detectSingleFace(temp_img, options)
-    return new Promise((resolve, reject)=>{
-      if(image_result){
-        const _x = image_result.box['x']-image_result.box['width']/20
-        const _y = image_result.box['y']-image_result.box['height']/20
-        const _width = image_result.box['width']*1.1
-        const _height = image_result.box['height']*1.1     
-        let crop_canvas = document.createElement('canvas')
-          crop_canvas.width = _width
-          crop_canvas.height = _height
-        let ctx = crop_canvas.getContext('2d')
-          ctx.drawImage(temp_img, _x, _y, _width, _height, 0, 0, _width, _height)
-        let croppedImgdata = crop_canvas.toDataURL('image/jpeg')
-        const obj_to_return = {status: true, imgData: croppedImgdata}
-        resolve(obj_to_return)
-      } else{
-        reject(false)
-      }
-    })
-  }
+$(document).ready(()=>{
+  // setInterval(async ()=>{ console.log(1)
+  //     // const a = await checkSingleFrame()
+  //     // try {
+  //     //   console.log(a)
+  //     // } catch (error) {
+  //     //   console.log(error)
+  //     // }
+  //     console.log(1)
+  //   }, 100)
+})
 
-  const detectImage = async ()=>{
-    const number_of_iterations = 10
-    let i = 0
-    while(i<number_of_iterations){
-      try {
-        const get_frame = await checkSingleFrame()
-        return get_frame
-      } catch (error) {
-        // console.log(error)
-        i++
-      } 
-    }
-    if(i === number_of_iterations){
-      document.getElementById('err_msg').innerText = "Image Not Detected"
-      $('#not-detected-div').removeClass('d-none')
-      setTimeout(()=>{
-        $('#not-detected-div').addClass('d-none')
-        document.getElementById('err_msg').innerText = ""
-      }, 2000)
-      return false
-    }
-  }
+ 
 
 
-const verify = async ()=>{
+
+const verify = async (clean_data)=>{
+  sessionStorage.setItem('verify_status', true)
+  sessionStorage.setItem('img', clean_data.imgData)
+  console.log(clean_data.imgData)
 
   const save_temp_data = async ()=>{
-    const detectedImage = await detectImage()
-    if(detectedImage){
-      const fileName = await setImageName()
-      localStorage.setItem('imageName', fileName)
-      localStorage.setItem('imageData', detectedImage.imgData)
-      $('#spinner').removeClass('d-none')
-      return true
-    }
-    return false
+    // const detectedImage = await detectImage()
+    // if(detectedImage){
+    //   const fileName = await setImageName()
+    //   localStorage.setItem('imageName', fileName)
+    //   localStorage.setItem('imageData', detectedImage.imgData)
+    //   $('#spinner').removeClass('d-none')
+    //   return true
+    // }
+    // return false
   }
 
   const ec2_call = (p)=>{
@@ -175,37 +180,37 @@ const verify = async ()=>{
       "collectionId": "testCollection"
     };
 
-    s3.uploadS3(params, (data, err)=>{
-      if(err){
-        document.getElementById('err_msg').innerText = "Error in Image Upload to Server"
-        $('#not-detected-div').removeClass('d-none')
-        setTimeout(()=>{
-          $('#not-detected-div').addClass('d-none')
-          document.getElementById('err_msg').innerText = ""
-        }, 2000)
-        console.log('error in upload s3', err)
-      } else {
-        ec2_call(params2)
-          .then((res)=>{
-            if(res === false){
-              document.getElementById('err_msg').innerText = "Failed to connect server"
-              $('#not-detected-div').removeClass('d-none')
-              setTimeout(()=>{
-                $('#not-detected-div').addClass('d-none')
-                document.getElementById('err_msg').innerText = ""
-                $('#spinner').addClass('d-none')
-              }, 2000)
-            }
-            else if(response.data.found){
-              document.getElementById('name').innerHTML = response.data.name
-              document.getElementById('email').innerHTML = response.data.email
-              document.getElementById('number').innerHTML = response.data.number
-            } else{
-              // detected face not registered
-            }
-          })
-      }
-    })
+    // s3.uploadS3(params, (data, err)=>{
+    //   if(err){
+    //     document.getElementById('err_msg').innerText = "Error in Image Upload to Server"
+    //     $('#not-detected-div').removeClass('d-none')
+    //     setTimeout(()=>{
+    //       $('#not-detected-div').addClass('d-none')
+    //       document.getElementById('err_msg').innerText = ""
+    //     }, 2000)
+    //     console.log('error in upload s3', err)
+    //   } else {
+    //     ec2_call(params2)
+    //       .then((res)=>{
+    //         if(res === false){
+    //           document.getElementById('err_msg').innerText = "Failed to connect server"
+    //           $('#not-detected-div').removeClass('d-none')
+    //           setTimeout(()=>{
+    //             $('#not-detected-div').addClass('d-none')
+    //             document.getElementById('err_msg').innerText = ""
+    //             $('#spinner').addClass('d-none')
+    //           }, 2000)
+    //         }
+    //         else if(response.data.found){
+    //           document.getElementById('name').innerHTML = response.data.name
+    //           document.getElementById('email').innerHTML = response.data.email
+    //           document.getElementById('number').innerHTML = response.data.number
+    //         } else{
+    //           // detected face not registered
+    //         }
+    //       })
+    //   }
+    // })
 
   } else {
 
@@ -258,8 +263,55 @@ document.getElementById('ok-btn').addEventListener("click", (event)=>{
   if(name == "" || email == "" || number == ""){
     console.log("please fill all the details")
   } else {
-    
+
   }
 
 
 })
+
+
+// const setImageName = async ()=>{
+//   try {
+//   //   const storeId = await localStorage.getItem("storeId")
+//     const storeId = "THN003"
+//     let d = new Date()
+//     let year = d.getUTCFullYear().toString()
+//     let month = d.getUTCMonth() + 1
+//       if(month < 10){month = '0'+month.toString()}
+//       else{month = month.toString()}
+//     let date = d.getUTCDate().toString()
+//     let hours = d.getUTCHours()
+//     let minute = d.getUTCMinutes()
+//     let second = d.getUTCSeconds()
+//     return storeId.toString() + date + month + year + hours + minute + second
+//   } catch (error) {
+//     return false
+//   }
+// }
+
+
+
+
+
+// const detectImage = async ()=>{
+//   const number_of_iterations = 10
+//   let i = 0
+//   while(i<number_of_iterations){
+//     try {
+//       const get_frame = await checkSingleFrame()
+//       return get_frame
+//     } catch (error) {
+//       // console.log(error)
+//       i++
+//     } 
+//   }
+//   if(i === number_of_iterations){
+//     document.getElementById('err_msg').innerText = "Image Not Detected"
+//     $('#not-detected-div').removeClass('d-none')
+//     setTimeout(()=>{
+//       $('#not-detected-div').addClass('d-none')
+//       document.getElementById('err_msg').innerText = ""
+//     }, 2000)
+//     return false
+//   }
+// }
